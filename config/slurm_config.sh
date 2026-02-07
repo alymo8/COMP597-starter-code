2b27e8968e0c563c5d669229cb20a8b017ea16a8
alymo8 <aly.mohamed@mail.mcgill.ca>
2026-02-06 19:53:41 -0500

    feat: add initial SLURM configuration script

---
+++ config/slurm_config.sh
@@ -0,0 +1,8 @@
+if [ "${BASH_SOURCE[0]}" -ef "$0" ]
+then
+    echo "'${BASH_SOURCE[0]}' is a config file, it should not be executed. Source it to populate the variables."
+    exit 1
+fi
+
+# Override SLURM defaults for this repo.
+export COMP597_SLURM_TIME_LIMIT="45:00"
